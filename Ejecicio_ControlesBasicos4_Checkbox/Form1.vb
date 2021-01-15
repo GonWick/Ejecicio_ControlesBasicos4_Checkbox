cc4d0e47710ccfcf3324bc5261f23c31414eed1b
GonWick <gonxza@gmail.com>
2021-01-15 14:08:52 +0100

    Retoques varios, listo para el proximo dia

--- Ejecicio_ControlesBasicos4_Checkbox/Form1.vb
+++ Ejecicio_ControlesBasicos4_Checkbox/Form1.vb
@@ -19,4 +19,27 @@
         CheckBox1.ThreeState = True
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        DateTimePicker1.Format = DateTimePickerFormat.Short
+        DateTimePicker1.Value = "31/12/2010"
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CheckedListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim i As Integer
+        ProgressBar1.Minimum = 0
+        ProgressBar1.Maximum = 200
+        For i = 0 To 200
+            ProgressBar1.Value = i
+        Next
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox1.Multiline = True
+        TextBox1.ScrollBars = ScrollBars.Both
+    End Sub
 End Class
