2c096ba74dce4fdd378a31d463ccb9146e128bd8
GonWick <gonxza@gmail.com>
2021-01-15 13:38:04 +0100

    retoque

--- Ejecicio_ControlesBasicos4_Checkbox/Form1.Designer.vb
+++ Ejecicio_ControlesBasicos4_Checkbox/Form1.Designer.vb
@@ -109,6 +109,7 @@ Partial Class Form1
         'CheckedListBox1
         '
         Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Items.AddRange(New Object() {"Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes"})
         Me.CheckedListBox1.Location = New System.Drawing.Point(494, 122)
         Me.CheckedListBox1.Name = "CheckedListBox1"
         Me.CheckedListBox1.Size = New System.Drawing.Size(208, 112)
